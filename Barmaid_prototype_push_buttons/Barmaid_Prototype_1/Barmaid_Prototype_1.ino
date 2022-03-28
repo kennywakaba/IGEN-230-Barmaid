47b22530ba40e58ffc1f5717112a231d14151703
Kenny Wakaba <wakabakenny@gmail.com>
2022-03-27 20:53:15 -0700

    sequential firmware works. use the pushbutton as a reference

--- Barmaid_prototype_push_buttons/Barmaid_Prototype_1/Barmaid_Prototype_1.ino
+++ Barmaid_prototype_push_buttons/Barmaid_Prototype_1/Barmaid_Prototype_1.ino
@@ -3,24 +3,34 @@
 #include <AccelStepper.h>
 
 //pins:
-const int HX711_dout = 5; //mcu > HX711 dout pin
-const int HX711_sck = 4; //mcu > HX711 sck pin
+const int HX711_dout = 4; //mcu > HX711 dout pin
+const int HX711_sck = 5; //mcu > HX711 sck pin
 const int RST = 2;
 
-const int valve = 3;
+const int valve1 = 8;
+const int valve2 = 9;
+const int valve3 = 10;
+const int valve4 = 11;
+const int valve5 = 12;
+const int valve6 = 13;
 
-const int dirPin = 6;
-const int stepPin = 7;
-const int endPin = 8;
+const int dirPin = 7;
+const int stepPin = 6;
+const int endPin = 13;
 
-const int pushA = 11;
-const int pushB = 12;
+const int pushBottom = 2;
+const int pushTop = 3;
 
 float weight;
-long position1 = 0;
-long position2 = 2000;
+long origin = 0;
+long station1 = 400;
+long station2 = 940;
+long station3 = 1460;
+long station4 = 1990;
+long station5 = 2510;
+long station6 = 2975
 
-boolean valve_on = 0;
+int sequence1 = 0;
 
 //HX711 constructor:
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
@@ -34,8 +44,12 @@ void RESET() {
   Serial.println("Reset");
   LoadCell.start(2000, true);
 
-  digitalWrite(valve, HIGH);
-  valve_on = 0;
+  digitalWrite(valve6, HIGH);
+  digitalWrite(valve5, HIGH);
+  digitalWrite(valve4, HIGH);
+  digitalWrite(valve3, HIGH);
+  digitalWrite(valve2, HIGH);
+  digitalWrite(valve1, HIGH);
 }
 
 void ENDSTOP() {
@@ -43,7 +57,7 @@ void ENDSTOP() {
   stepper.setCurrentPosition(0);
 }
 
-void DriveMotor(long Position) {
+void DRIVE(long Position) {
   stepper.enableOutputs();
   stepper.moveTo(Position);
   while (stepper.currentPosition() != Position) {
@@ -74,16 +88,17 @@ void setup() {
     LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
     Serial.println("Startup is complete");
   }
-  
+
   // MOTOR SETUP
-  stepper.setMaxSpeed(2000);
-  stepper.setSpeed(2000);
-  stepper.setAcceleration(200);
+  stepper.setMaxSpeed(1400);
+  stepper.setSpeed(1400);
+  stepper.setAcceleration(1200);
   stepper.setCurrentPosition(0);
   stepper.disableOutputs();
 
   // VALVE SETUP
-  pinMode(valve, OUTPUT);
+  pinMode(valve2, OUTPUT);
+  pinMode(valve4, OUTPUT);
   pinMode(RST, INPUT);
   pinMode(endPin, INPUT);
   //attachInterrupt (0, RESET, RISING);
@@ -93,8 +108,11 @@ void loop() {
   static boolean newDataReady = 0;
   const int serialPrintInterval = 0; //increase value to slow down serial print activity
 
-  if (stepper.currentPosition() != position2 || weight > 45) {
-    digitalWrite(valve, HIGH);
+  if (stepper.currentPosition() != station2 || weight > 10) {
+    digitalWrite(valve2, HIGH);
+  }
+  if (stepper.currentPosition() != station4 || weight > 10) {
+    digitalWrite(valve4, HIGH);
   }
 
   // check for new data/start next conversion:
@@ -103,33 +121,53 @@ void loop() {
   // get smoothed value from the dataset:
   if (newDataReady) {
     if (millis() > t + serialPrintInterval) {
-      weight = LoadCell.getData() * -1;
+      weight = LoadCell.getData();
       Serial.print("Load_cell output val: ");
       Serial.println(weight);
       newDataReady = 0;
       t = millis();
 
-      if (digitalRead(pushA) == HIGH) {
-        DriveMotor(position1);
+      if (digitalRead(pushBottom) == HIGH) { // black button
+        DRIVE(station4);
+        sequence1 = 1;
+        RESET();
       }
-      if (digitalRead(pushB) == HIGH) {
-        DriveMotor(position2);
-        valve_on = 1;
-      }
-
-      if (digitalRead(endPin) == HIGH) { // indicating the origin 
-        ENDSTOP();
-      }
-
-      if (stepper.currentPosition() == position2 && weight < 45 && valve_on == 1) {
-        digitalWrite(valve, LOW);
-      }
-      else if (stepper.currentPosition() == position2 && weight > 45) {
-        digitalWrite(valve, HIGH);
+      if (digitalRead(pushTop) == HIGH) { // red button
+        DRIVE(station4);
+        sequence1 = 1;
         RESET();
       }
 
+      if (digitalRead(endPin) == HIGH) { // indicating the origin
+        //ENDSTOP();
+      }
+
+      // sequence 1: station4 -> station2
+      // note the order of operation is reversed, this is due to generalized if statements that simplifies variable manipulations
+      if (sequence1 = 1) {
+        if (stepper.currentPosition() == station2 && weight < 10) { // step 2
+          digitalWrite(valve2, LOW);
+        }
+        else if (stepper.currentPosition() == station2 && weight > 10) {
+          digitalWrite(valve2, HIGH);
+          delay(5000); // wait for the leakage to end
+          DRIVE(origin);
+          sequence1 = 0;
+          RESET();
+        }
+        if (stepper.currentPosition() == station4 && weight < 10) { // step 1
+          digitalWrite(valve4, LOW);
+        }
+        else if (stepper.currentPosition() == station4 && weight > 10) {
+          digitalWrite(valve4, HIGH);
+          delay(5000); // wait for the leakage to end
+          DRIVE(station2);
+          RESET();
+        }
+      }
+    // sequence 1: 4 -> 2
+
+    
     }
   }
-
 }
